8ec0105b31dff0bb5b0728b37e32837906408cf5
Katoos <ahmedkatoos10@gmail.com>
2017-04-30 12:28:16 +0200

    Add files via upload

---
+++ Irrigation_System_Final.ino
@@ -0,0 +1,61 @@
+const int LED1= 12;
+const int LED2= 2;
+const int LED3= 8;
+#define RELAY1  7      
+void setup(){
+Serial.begin(9600);
+pinMode(LED1,OUTPUT);
+pinMode(LED2,OUTPUT);
+pinMode(LED3,OUTPUT);
+pinMode(RELAY1, OUTPUT); 
+}
+void loop() {
+int SensorReading= analogRead(A0);
+Serial.println (SensorReading);
+delay(1000); 
+if (SensorReading >= 650 && SensorReading < 999){
+digitalWrite(LED1,HIGH);
+delay(500);
+digitalWrite(LED1,LOW);
+delay(500);
+digitalWrite(LED2,LOW);
+digitalWrite(LED3,LOW);
+digitalWrite(RELAY1,0);          
+Serial.println("WATER ON");  
+}     
+if (SensorReading > 200 && SensorReading < 650){
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED3,LOW);
+  digitalWrite(RELAY1,1);          
+Serial.println("WATER OFF");
+
+}
+if (SensorReading > 1000 )
+{
+int Value = analogRead(A1);
+float Celsius = ((Value/1024.0)*5000)/10; 
+Serial.print("TEMPRATURE = ");
+Serial.print(Celsius);
+Serial.print("*C");
+Serial.println();
+delay(500);           
+digitalWrite(LED1,LOW);
+digitalWrite(LED2,LOW);
+digitalWrite(LED3,HIGH);
+if (Value > 30 && Value < 50){
+digitalWrite(RELAY1,0);
+delay (3600000);
+digitalWrite ( RELAY1,1);
+delay (86400000);
+}
+if (Value > 10 && Value < 30){
+digitalWrite(RELAY1,0);
+delay (1550000);
+digitalWrite ( RELAY1,1);
+delay (86400000);
+}
+}
+}
+
+  
---
+++ Motion_Sensor_Final.ino
@@ -0,0 +1,58 @@
+int motion = 0;
+// defines pins numbers
+const int trig = 9;
+const int echo = 10;
+const int led = 4;
+
+// defines variables
+long duration;
+int distance;
+int safetyDistance;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2, INPUT);
+pinMode(8,OUTPUT);
+pinMode(trig, OUTPUT); // Sets the trigPin as an Output
+pinMode(echo, INPUT); // Sets the echoPin as an Input
+pinMode(led, OUTPUT);
+Serial.begin(9600); // Starts the serial communication
+}
+void loop()
+{
+  // put your main code here, to run repeatedly:
+motion = digitalRead(2);
+if (motion == HIGH) {
+  digitalWrite(8, HIGH);
+  delay(5000);
+}
+else {
+  digitalWrite(8, LOW);
+}
+// Clears the trigPin
+digitalWrite(trig, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trig, HIGH);
+delayMicroseconds(10);
+digitalWrite(trig, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echo, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+
+safetyDistance = distance;
+if (safetyDistance > 20){
+  digitalWrite(led, LOW);
+}
+if (safetyDistance > 10 && safetyDistance < 20)
+{
+  digitalWrite(led, HIGH);
+}
+
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+}
